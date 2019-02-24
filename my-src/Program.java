3e5e41de362855b0fea312cae27ae9da78ff0faf
Theodor Thornhill <theodorthornhill@icloud.com>
2019-02-24 10:04:37 +0100

    changed the list implementation for Program

--- my-src/Program.java
+++ my-src/Program.java
@@ -2,19 +2,18 @@ import java.util.*;
 
 public class Program {
     String name;
-    List<LinkedList> declarations;
-    public Program(String name, LinkedList<LinkedList> declarations) {
+    List<String> declarations;
+    public Program(String name, LinkedList<String> declarations) {
         this.name = name;
         this.declarations = declarations;
     }
 
     public String printAst() {
         StringBuilder sb = new StringBuilder();
-        sb.append("(PROGRAM ");
-        sb.append("(NAME ");
+        sb.append("(PROGRAM (NAME ");
         sb.append(this.name);
         sb.append(")\n");
-        sb.append(declarations.toString());
+        sb.append(declarations);
         sb.append("\n)\n");
         return sb.toString();
     }
