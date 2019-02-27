297b0d025f94d2c55dcd9d126bca5f0e0b7019c9
Theodor Thornhill <theodorthornhill@icloud.com>
2019-02-27 22:48:39 +0100

    added call-list

--- my-src/Call.java
+++ my-src/Call.java
@@ -1,11 +1,21 @@
+import java.util.*;
+
 public class Call extends Stmt {
-    public Call(String name) {
+    LinkedList<Expr> el;
+    public Call(String name, LinkedList<Expr> el) {
         this.name = name;
+        this.el = el;
     }
 
     public String printAst(int indentLevel) {
         StringBuilder sb = new StringBuilder();
         sb.append("(CALL " + name);
+        if (el != null) {
+            for (Expr e : el) {
+                sb.append("\n" + Main.buildIndentation(indentLevel+1) + "(" + e);
+                sb.append("\n");                
+            }            
+        }
         sb.append("\n" + Main.buildIndentation(indentLevel) + ")");
         return sb.toString();
     }
