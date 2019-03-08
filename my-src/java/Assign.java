e0c149a8f2ba41fd07363ef33327d8443a6dc474
Theodor Thornhill <theodorthornhill@icloud.com>
2019-03-08 12:17:52 +0100

    assign expressions.

--- my-src/java/Assign.java
+++ my-src/java/Assign.java
@@ -1,14 +1,13 @@
 public class Assign extends Stmt {
-    public Assign(String name, Object e) {
-        this.name = name;
+    public Assign(Object name, Object e) {
+        this.e2 = name;
         this.e = e;
     }
 
     public String printAst(int indentLevel) {
         StringBuilder sb = new StringBuilder();
         sb.append("(ASSIGN_STMT");
-        sb.append(PrintHelper.newlineAndIndentWithHelper(PrintHelper.printName(name),
-                                                         indentLevel+1));
+        sb.append(PrintHelper.newlineAndIndentWithHelper(e2, indentLevel+1));
         sb.append(PrintHelper.newlineAndIndentWithHelper(e, indentLevel+1));
         sb.append(PrintHelper.endWithParen(indentLevel));
         return sb.toString();
--- my-src/java/Stmt.java
+++ my-src/java/Stmt.java
@@ -2,6 +2,7 @@ import java.util.*;
 public abstract class Stmt {
     String name;
     Object e;
+    Object e2;
     LinkedList<Stmt> sl;
     public abstract String printAst(int indentLevel);
 }
