ebf593266b4634deb8daeaa303b1a757d0f94f87
Theodor Thornhill <theodorthornhill@icloud.com>
2019-04-23 22:43:55 +0200

    Add first reasonable delegation from codegenerationhelper

--- src/java/programlogic/CodeGenerationHelper.java
+++ src/java/programlogic/CodeGenerationHelper.java
@@ -2,14 +2,21 @@ import bytecode.*;
 import bytecode.type.*;
 import bytecode.instructions.*;
 
+// The idea of this helper class is to act like a factory for exprs. The problem
+// with the expressions is that they can be a statement or expression. Therefore
+// I need an elegant way to assign the values. 
 public class CodeGenerationHelper {
     public static void exprHelper(CodeProcedure proc, Object node) {
         if (node instanceof BinaryExpr)
             ((BinaryExpr) node).generateCode(proc);
         // else if (node instanceof Call)
         //     ((Call) node).generateCode(proc);
-        else if (node instanceof Literal)
-            ((Literal) node).generateCode(proc);
+        else if (node instanceof Literal) {
+            // Get the literal and add it as a variable. Good for now, but must
+            // be changed later I believe.
+            Literal lit = ((Literal) node);
+            proc.addLocalVariable(lit.getValue().toString(), lit.getCodeType());
+        }
         else
             System.out.println("Bug in Codegenerationhelper");
     }
