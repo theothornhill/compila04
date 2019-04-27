fddcdb6386f7862c75eead981360cc43809e86af
Theodor Thornhill <theodorthornhill@icloud.com>
2019-04-27 09:02:36 +0200

    make name of declarations public

--- src/java/declarations/Decl.java
+++ src/java/declarations/Decl.java
@@ -1,7 +1,7 @@
 import bytecode.*;
 
 public abstract class Decl {
-    String name;
+    public String name;
     public abstract String printAst(int indentLevel);
     public abstract void generateCode(CodeFile codeFile);
 }
