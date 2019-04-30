4a5c74cf9ec2906afa0668074ffed8d9ba9cbd8e
Theodor Thornhill <theodorthornhill@icloud.com>
2019-04-30 19:35:57 +0200

    New symboltable class

---
+++ src/java/programlogic/SymbolTable.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+
+public class SymbolTable {
+
+    public HashMap<String, String> table;
+
+    public SymbolTable() {
+        table = new HashMap<String, String>();
+    }
+
+    public void insert() {
+        
+    }
+
+    public HashMap lookup() {
+        return null;
+    }
+        
+}
