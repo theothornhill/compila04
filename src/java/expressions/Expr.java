11cc5fe621c21d9e24f9c3e81c5d2297511acd53
Theodor Thornhill <theodorthornhill@icloud.com>
2019-04-30 22:22:35 +0200

    declare abstract method for symbol table in expr

--- src/java/expressions/Expr.java
+++ src/java/expressions/Expr.java
@@ -2,7 +2,9 @@ import bytecode.*;
 import bytecode.type.*;
 public abstract class Expr {
     Object expr;
+    SymbolTable table = new SymbolTable();
     public abstract String printAst(int indentLevel);
+    public abstract void addToSymbolTable();
     public abstract void generateCode(CodeProcedure proc);
 
 }
