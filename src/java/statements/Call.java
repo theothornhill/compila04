0374979d3a3e80c343666fd3c585245a305d5f51
Theodor Thornhill <theodorthornhill@icloud.com>
2019-05-01 15:36:57 +0200

    use codegenerationhelper

--- src/java/statements/Call.java
+++ src/java/statements/Call.java
@@ -13,7 +13,9 @@ public class Call extends Stmt {
     public void addToSymbolTable() {
         table.insert("Statement-type", "call");
         table.insert("Name", name);
-        el.stream().forEach(e -> table.insert(e.toString(), e));
+        el.stream()
+            .forEach(e -> table.insert(e.getClass().toString(),
+                                       CodeGenerationHelper.getTable(e)));
     }
 
     public void generateCode(CodeFile codeFile) {
--- src/java/statements/If.java
+++ src/java/statements/If.java
@@ -17,7 +17,7 @@ public class If extends Stmt {
 
     public void addToSymbolTable() {
         table.insert("Statement-type", "if");
-        table.insert("Expr", e);
+        table.insert("Expr", CodeGenerationHelper.getTable(e));
         sl.stream().forEach(s -> table.insert(s.toString(), s));
         sl2.stream().forEach(s -> table.insert(s.toString(), s));
     }
