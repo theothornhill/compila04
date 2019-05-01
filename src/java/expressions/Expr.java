1673e4cfa37238dd7d5ebf49e32a9c086fda6cb8
Theodor Thornhill <theodorthornhill@icloud.com>
2019-05-01 15:36:25 +0200

    add symboltable to expr

--- src/java/expressions/Expr.java
+++ src/java/expressions/Expr.java
@@ -2,9 +2,13 @@ import bytecode.*;
 import bytecode.type.*;
 public abstract class Expr {
     Object expr;
-    SymbolTable table = new SymbolTable();
+    public SymbolTable table = new SymbolTable();
     public abstract String printAst(int indentLevel);
     public abstract void addToSymbolTable();
     public abstract void generateCode(CodeProcedure proc);
+
+    public SymbolTable getTable() {
+        return table;
+    }
 
 }
