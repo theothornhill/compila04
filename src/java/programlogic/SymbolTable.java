33181953e1141f17d4e7f10b9eebb2e649be6e00
Theodor Thornhill <theodorthornhill@icloud.com>
2019-05-04 10:22:24 +0200

    possibly move away from this design

--- src/java/programlogic/SymbolTable.java
+++ src/java/programlogic/SymbolTable.java
@@ -5,13 +5,14 @@ import java.util.LinkedList;
 
 public class SymbolTable {
     
-    public Map<String, LinkedList<Object>> table;
+    public Map<String, Object> table;
     public int key;
         
     public SymbolTable(int key) {
         this.key = key;
-        this.table = new HashMap<String, LinkedList<Object>>();
-        this.table.put("" + this.key, new LinkedList<Object>());
+        this.table = new HashMap<String, Object>();
+        // this.table.put("" + this.key, new LinkedList<Object>());
+        // this.table.put("" + this.key, ));
     }
 
     public int getKey() {
@@ -29,14 +30,18 @@ public class SymbolTable {
     public void addTable() {
         // must be used after key as been incremented
         if (!table.containsKey("" + key)) 
-            table.put("" + key, new LinkedList<Object>());
+            // table.put("" + key, new LinkedList<Object>());
+            table.put("" + key, new HashMap<String, Object>());
+        else
+            table.put("" + key + "a", new LinkedList<Object>());
     }
 
     public void insert(Object val) {
-        this.table.get("" + this.key).add(val);
+        // this.table.get("" + this.key).add(val);
+        this.table.put("" + key, val);
     }
 
-    public LinkedList lookup(String key) {
+    public Object lookup(String key) {
          return this.table.get(key);
     }
 
