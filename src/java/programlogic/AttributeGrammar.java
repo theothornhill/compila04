763e384addb1af287a6caaf1e85b4b959c8303f9
Theodor Thornhill <theodorthornhill@icloud.com>
2019-05-04 10:18:52 +0200

    Attributegrammar interface

---
+++ src/java/programlogic/AttributeGrammar.java
@@ -0,0 +1,6 @@
+public interface AttributeGrammar {
+    public Object createdBy();
+    public void setCreatorOf();
+    public void setLexicalScopeLevel(int scope);
+    public void setCreatedBy(Object node);    
+}
