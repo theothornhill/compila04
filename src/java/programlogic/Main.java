154c16402a1e9d0fbbe45df39a70eda39656d935
Theodor Thornhill <theodorthornhill@icloud.com>
2019-05-04 10:22:56 +0200

    cleanup

--- src/java/programlogic/Main.java
+++ src/java/programlogic/Main.java
@@ -20,48 +20,6 @@ public class Main {
         try {
             lex = new Lexer(new FileReader(args[0]));
             parser = new parser(lex);
-
-            // // Testing code generation
-            // CodeFile codeFile = new CodeFile();
-            // codeFile.addProcedure("print_float");
-            // // I:
-            // codeFile.addProcedure("Main");
-            // codeFile.addProcedure("test");
-            // codeFile.addStruct("Complex");
-            // // II:
-            // CodeProcedure main = new CodeProcedure("Main", VoidType.TYPE,
-            //                                        codeFile);
-            // main.addInstruction(new RETURN());
-            // codeFile.updateProcedure(main);
-
-            // // IV:
-            // CodeProcedure test = new CodeProcedure("test", VoidType.TYPE,
-            //                                        codeFile);
-            // test.addParameter("firstPar", FloatType.TYPE);
-            // test.addParameter("secondPar", FloatType.TYPE);
-            // test.addInstruction(new LOADLOCAL(test.variableNumber("firstPar")));
-            // test.addInstruction(new CALL(test.procedureNumber("print_float")));
-            // test.addInstruction(new RETURN());
-            // codeFile.updateProcedure(test);
-
-            //     // V:
-            // CodeStruct complex = new CodeStruct("Complex");
-            // complex.addVariable("Real", FloatType.TYPE);
-            // complex.addVariable("Imag", FloatType.TYPE);
-            // codeFile.updateStruct(complex);
-            // // VI:
-            // CodeProcedure printFloat = new CodeProcedure("print_float",
-            //                                              VoidType.TYPE, codeFile);
-            // test.addParameter("f", FloatType.TYPE);
-            // codeFile.updateProcedure(printFloat);
-            // // VII:
-            // codeFile.setMain("Main");
-            // byte[] bytecode = codeFile.getBytecode();
-
-            // DataOutputStream stream = new DataOutputStream(new FileOutputStream("/Users/theodor/Dropbox/Studier/fag/INF5110/compila04/example.bin"));
-            // stream.write(bytecode);
-            // stream.close();
-            // end of test for code generation
             try {
                 returnSymbol = parser.parse();
                 Program program = (Program)returnSymbol.value;
@@ -81,9 +39,10 @@ public class Main {
                 stream.close();
                 // System.out.println(program.printAst());
                 VirtualMachine vm = new VirtualMachine("/Users/theodor/Dropbox/Studier/fag/INF5110/compila04/example.bin");
-                vm.list();
+                // vm.list();
 
             } catch (Exception ee) {
+                ee.printStackTrace();
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
