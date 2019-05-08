f7746619810bac0a5773ae287ec143ae72fefbf9
Theodor Thornhill <theodorthornhill@icloud.com>
2019-05-08 19:38:10 +0200

    cleanup the symbol table

--- src/java/programlogic/SymbolTable.java
+++ src/java/programlogic/SymbolTable.java
@@ -6,38 +6,12 @@ import java.util.LinkedList;
 public class SymbolTable {
     
     public Map<String, Object> table;
-    public int key;
         
     public SymbolTable() {
-        this.key = key;
         this.table = new HashMap<String, Object>();
-        // this.table.put("" + this.key, new LinkedList<Object>());
-        // this.table.put("" + this.key, ));
-    }
-
-    public int getKey() {
-        return this.key;
-    }
-
-    public void incrementKey() {
-        this.key++;
-    }
-
-    public void decrementKey() {
-        this.key--;
-    }
-
-    public void addTable() {
-        // must be used after key as been incremented
-        if (!table.containsKey("" + key)) 
-            // table.put("" + key, new LinkedList<Object>());
-            table.put("" + key, new HashMap<String, Object>());
-        else
-            table.put("" + key + "a", new LinkedList<Object>());
     }
 
     public void insert(Object val) throws Exception {
-        // this.table.get("" + this.key).add(val);
         if (this.table.containsKey(val.toString()))
             throw new Exception("Double declaration of symbol " + val);
         this.table.put(val.toString(), val); 
@@ -51,9 +25,7 @@ public class SymbolTable {
                 ? currentScope.table.get(key)
                 : null;
         }
-        // if (scope instanceof Stmt)
-        //     currentScope = ((Stmt)scope).getTable();
-        // if (scope instanceof Decl)
+
         currentScope = ((Decl)scope).getTable();
 
         return currentScope.table.containsKey(key)
@@ -63,7 +35,6 @@ public class SymbolTable {
 
     public String toString() {
         return Arrays.asList(this.table).toString();
-        // return Arrays.toString(table.entrySet().toArray());
     }
         
 }
