472d4b3e083301144f2ef9c8d24e3bb2cda55208
Theodor Thornhill <theodorthornhill@icloud.com>
2019-05-08 10:20:36 +0200

    we need to refer to symbol table

--- src/java/expressions/Expr.java
+++ src/java/expressions/Expr.java
@@ -6,7 +6,6 @@ public abstract class Expr {
     public int lexicalScopeLevel = 6;
     public SymbolTable table = new SymbolTable();
     public abstract String printAst(int indentLevel);
-    public abstract void typeCheck() throws Exception;
     public abstract void typeCheck(SymbolTable table, Object scope) throws Exception;
     public abstract void addToSymbolTable(SymbolTable table);
     public abstract void generateCode(CodeProcedure proc);
