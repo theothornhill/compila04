a2349824814f8ed5b49de1c56f34bd042f3970f4
Theodor Thornhill <theodorthornhill@icloud.com>
2019-05-11 17:44:12 +0200

    change error code in main.  result semantic/syntax error. Not done

--- src/java/programlogic/Main.java
+++ src/java/programlogic/Main.java
@@ -12,36 +12,42 @@ import runtime.*;
 
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        Lexer lex = null;
-        parser parser = null;
+
+    public static Program compile(String[] args) throws Exception {
+        Lexer lex = new Lexer(new FileReader(args[0]));
+        parser parser = new parser(lex);
         Symbol returnSymbol;
-        // Try to parse and return the Program as a Program
+        Program program = null;
+        int currentLevel = 0;
         try {
-            lex = new Lexer(new FileReader(args[0]));
-            parser = new parser(lex);
-            try {
-                returnSymbol = parser.parse();
-                Program program = (Program)returnSymbol.value;
-                // Add some custom things after parsing - in particular main-method
-                CodeFile codeFile = new CodeFile();
-
-                program.generateCode(codeFile);
-
-                byte[] bytecode = codeFile.getBytecode();
-
-                DataOutputStream stream = new DataOutputStream(new FileOutputStream("/Users/theodor/Dropbox/Studier/fag/INF5110/compila04/example.bin"));
-                stream.write(bytecode);
-                stream.close();
-                // System.out.println(program.printAst());
-                VirtualMachine vm = new VirtualMachine("/Users/theodor/Dropbox/Studier/fag/INF5110/compila04/example.bin");
-                vm.list();
-                vm.run();
-            } catch (Exception ee) {
-                ee.printStackTrace();
-            }
-        } catch (FileNotFoundException e) {
+            program = (Program)parser.parse().value;            
+        } catch (Exception e) {
             e.printStackTrace();
+        }
+        program.addToSymbolTable();
+        program.setCreatorOf();
+        program.setLexicalScopeLevel(currentLevel+1);
+        program.typeCheck();   
+        return program;
+    }
+
+    public static void main(String[] args) throws IOException {
+        try {
+            // Add some custom things after parsing - in particular main-method
+            CodeFile codeFile = new CodeFile();
+            Program program = compile(args);
+            program.generateCode(codeFile);
+
+            byte[] bytecode = codeFile.getBytecode();
+
+            DataOutputStream stream = new DataOutputStream(new FileOutputStream("/Users/theodor/Dropbox/Studier/fag/INF5110/compila04/example.bin"));
+            stream.write(bytecode);
+            stream.close();
+            VirtualMachine vm = new VirtualMachine("/Users/theodor/Dropbox/Studier/fag/INF5110/compila04/example.bin");
+            vm.list();
+            vm.run();
+        } catch (Exception ee) {
+            ee.printStackTrace();
         }
     }
 }
